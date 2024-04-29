eb79df6ccf4006ee44601661b70766b5f26befe2
ThinkPad Rene <rene.dorsch@iis.fraunhofer.de>
2024-04-29 17:33:54 +0200

    Updated readme and added docu to bluetooth evaluation

---
+++ evaluation/effectiveness-evaluation/bluetooth-scraper/bluetooth-scrapper/bluetooth_scrapper/download_database_entries.py
@@ -0,0 +1,52 @@
+import requests
+import json
+
+# The URL is constructed from the scheme, host, and filename
+url = "https://platformapi.bluetooth.com/api/platform/Listings/Search"
+
+
+# Request payload
+payload = {
+    "searchString": None,
+    "searchQualificationsAndDesigns": True,
+    "searchDeclarationOnly": True,
+    "searchEndProductList": False,
+    "searchPRDProductList": True,
+    "searchMyCompany": False,
+    "productTypeId": 0,
+    "icsItem": "",
+    "specName": 0,
+    "bqaApprovalStatusId": -1,
+    "bqaLockStatusId": -1,
+    "listingDateEarliest": "2023-03-05",
+    "listingDateLatest": "2024-03-05",
+    "userId": 0,
+    "memberId": None,
+    "layers": [],
+    "maxResults": 50000
+}
+
+
+year_range = 2025 - 1999
+
+for i in range(year_range):
+    earliest_date = f"{2025 - i}-01-01"
+    latest_date = f"{2025 - i}-12-31"
+    payload["listingDateEarliest"] = earliest_date
+    payload["listingDateLatest"] = latest_date
+        
+    # Make the POST request
+    response = requests.post(url, json=payload)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Print the response content
+        print(f"Response from server for {latest_date} to {earliest_date}:")
+        print(len(response.json()))
+        dump = json.dumps(response.json(), indent=4)
+        #print(dump)
+        with open(f'../../data/bluetooth_devices/{earliest_date}_{latest_date}.json', 'w') as f:
+            f.write(dump)
+    else:
+        print(f"Failed to retrieve data, status code: {response.status_code}")
+
---
+++ evaluation/effectiveness-evaluation/bluetooth-scraper/bluetooth-scrapper/bluetooth_scrapper/download_device_details.py
@@ -0,0 +1,100 @@
+import asyncio
+import json
+import os
+import aiofiles
+import requests
+from typing import Optional
+
+
+def fetch_json_for_listing_id(listing_id: str) -> Optional[dict]:
+    """
+    Fetches JSON data for a given listing ID from the Bluetooth platform API.
+
+    Args:
+        listing_id (str): The ID of the listing to fetch JSON data for.
+
+    Returns:
+        dict or None: The JSON data as a dictionary if the request was successful, 
+        otherwise None.
+
+    """
+    url = f"https://platformapi.bluetooth.com/api/Platform/ListingDetails/{listing_id}"
+
+    # Make the GET request
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    
+    return None
+
+async def save_json_content(listing_id: str, content: dict) -> None:
+    """
+    Save the JSON content to a file.
+
+    Args:
+        listing_id (str): The ID of the listing.
+        content (dict): The JSON content to be saved.
+
+    Returns:
+        None
+    """
+    filename = f'../../data/device_details/{listing_id}.json'
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    async with aiofiles.open(filename, 'w') as file:
+        await file.write(json.dumps(content, indent=4))
+
+def get_existing_ids():
+    """
+    Retrieves the existing IDs from the 'device_details' folder.
+
+    Args:
+        None
+        
+    Returns:
+        list: A list of existing IDs extracted from the filenames in the 'device_details' folder.
+    """
+    folder_path = '../../data/device_details'
+    files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
+    return [f.split('.')[0] for f in files]
+
+async def process_json_file(filepath):
+    """
+    Process a JSON file asynchronously.
+
+    Args:
+        filepath (str): The path to the JSON file.
+
+    Returns:
+        None
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        JSONDecodeError: If the file content is not a valid JSON.
+
+    """
+    async with aiofiles.open(filepath, 'r') as file:
+        content = await file.read()
+        data = json.loads(content)
+        existing_ids = get_existing_ids()
+        print(existing_ids)
+        for item in data:
+            listing_id = item.get('ListingId')
+            if listing_id:
+                if str(listing_id) in existing_ids:
+                    print(f'Already found device for listing id: {listing_id}')
+                    continue
+                json_content = fetch_json_for_listing_id(listing_id)
+                print(f'Found device for listing id: {listing_id}')
+                await save_json_content(listing_id, json_content)
+
+async def main():
+    json_folder_path = '../../data/bluetooth_devices'
+    json_files = [f for f in os.listdir(json_folder_path) if f.endswith('.json')]
+    for json_file in json_files:
+        await process_json_file(os.path.join(json_folder_path, json_file))
+
+asyncio.run(main())
